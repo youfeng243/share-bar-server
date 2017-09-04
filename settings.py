aeca873688b7a176a5328e41637c19c504cdfb78
youfeng <youfeng@haizhi.com>
2017-09-04 20:46:56 +0800

    app配置文件确认

---
+++ settings.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+@author: youfeng
+@email: youfeng243@163.com
+@license: Apache Licence
+@file: settings.py
+@time: 2017/9/4 20:30
+"""
+
+DEBUG = True
+TESTING = 1
+SECRET_KEY = 'hard to guess..'
+SQLALCHEMY_DATABASE_URI = 'mysql://root:555556@localhost:3306/share_bar_db'
+SQLALCHEMY_COMMIT_ON_TEARDOWN = True
+SQLALCHEMY_TRACK_MODIFICATIONS = True
