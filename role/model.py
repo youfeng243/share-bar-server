133368c5121f7411abba127b4bc8123535506c2c
youfeng <youfeng@haizhi.com>
2017-09-05 21:54:54 +0800

    管理员角色管理表

---
+++ role/model.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+@author: youfeng
+@email: youfeng243@163.com
+@license: Apache Licence
+@file: role.py
+@time: 2017/8/30 09:06
+"""
+from datetime import datetime
+
+from exts.database import db
+
+
+# 管理员角色管理
+class Role(db.Model):
+    __tablename__ = 'role'
+
+    # ID
+    id = db.Column(db.Integer, primary_key=True)
+
+    # 角色名称
+    name = db.Column(db.String(64), unique=True, nullable=False, index=True)
+
+    # todo 角色权限 这里需要角色的权限管理，目前没想好先预留
+
+    # 创建时间
+    ctime = db.Column(db.DateTime(), default=datetime.utcnow)
+
+    # 数据更新时间
+    utime = db.Column(db.DateTime(), default=datetime.utcnow)
+
+    @classmethod
+    def create(cls, name):
+        role = cls(
+            name=name)
+        db.session.add(role)
+        db.session.commit()
+        return role
+
+    @classmethod
+    def get(cls, a_id):
+        return cls.query.get(a_id)
+
+    @classmethod
+    def get_all(cls):
+        return cls.query.all()
+
+    # 删除数据
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    # 更新下修改时间
+    def save(self):
+        self.utime = datetime.now()
+        db.session.add(self)
+        db.session.commit()
+
+    def __repr__(self):
+        return '<Role {}>'.format(self.role_name)
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'utime': self.utime,
+            'ctime': self.ctime,
+        }
