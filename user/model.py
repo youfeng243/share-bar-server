7b4e40bcfa16d6d04660318f5408d6e084317b81
youfeng <youfeng@haizhi.com>
2017-09-05 22:33:06 +0800

    完成用户数据模型定义

---
+++ user/model.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+@author: youfeng
+@email: youfeng243@163.com
+@license: Apache Licence
+@file: user.py
+@time: 2017/8/29 17:58
+"""
+from datetime import datetime
+
+from exts.database import db
+
+
+class User(db.Model):
+    __tablename__ = 'user'
+
+    # 使用状态
+    STATE_VALUES = ('unused', 'using')
+
+    # ID
+    id = db.Column(db.Integer, primary_key=True)
+
+    # 用户名
+    username = db.Column(db.String(128), unique=True, index=True, nullable=False)
+
+    # 密码
+    password = db.Column(db.String(128), nullable=False)
+
+    # 电话号码
+    telephone = db.Column(db.String(64), unique=True, nullable=False)
+
+    # 总充值金额
+    total_account = db.Column(db.Integer, nullable=False, default=0)
+
+    # 总的消费金额
+    used_account = db.Column(db.Integer, nullable=False, default=0)
+
+    # 当前用户使用状态信息 unused 离线  using 在线
+    state = db.Column(db.Enum(*STATE_VALUES), index=True, default='unused')
+
+    # 创建时间
+    ctime = db.Column(db.DateTime(), default=datetime.utcnow)
+
+    # 数据更新时间
+    utime = db.Column(db.DateTime(), default=datetime.utcnow)
+
+    # 删除用户
+    deleted = db.Column(db.Boolean, default=False)
+
+    # 禁止用户 如用户违反规定则停用该用户
+    forbid = db.Column(db.Boolean, default=False)
+
+    @classmethod
+    def create(cls, username, password, telephone):
+        user = cls(
+            username=username,
+            password=password,
+            telephone=telephone)
+        db.session.add(user)
+        db.session.commit()
+        return user
+
+    @classmethod
+    def get(cls, a_id):
+        return cls.query.get(a_id)
+
+    @classmethod
+    def get_all(cls):
+        return cls.query.all()
+
+    # 删除用户
+    def delete(self):
+        self.deleted = True
+        self.save()
+
+    # 禁止用户
+    def forbidden(self):
+        self.forbid = True
+        self.save()
+
+    # 更新下修改时间
+    def save(self):
+        self.utime = datetime.now()
+        db.session.add(self)
+        db.session.commit()
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'username': self.username,
+            'password': self.password,
+            'telephone': self.telephone,
+            'total_account': self.total_account,
+            'used_account': self.used_account,
+            'state': self.state,
+            'ctime': self.ctime,
+            'utime': self.utime,
+            'forbid': self.forbid,
+            'deleted': self.deleted,
+        }
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
+
+
+db.create_all()
