c62bf3d26a2436918bade93bbd61889ea8048d7f
youfeng <youfeng@haizhi.com>
2017-09-20 09:09:49 +0800

    增加判断用户是否已经登录

--- service/admin/view.py
+++ service/admin/view.py
@@ -10,7 +10,6 @@
 from flask import Blueprint
 from flask import g
 from flask import request
-from flask import session
 from flask_login import current_user
 from flask_login import login_required
 from flask_login import login_user
