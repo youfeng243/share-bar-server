8f7ee9b0f6c88e61bb8991cc1024c2d8f6108ba9
youfeng <youfeng@haizhi.com>
2017-09-21 23:20:19 +0800

    修正少量BUG

--- exts/sms.py
+++ exts/sms.py
@@ -21,7 +21,7 @@ def sms_default_callback(session, resp):
 def request_sms(mobile):
     if settings.DEBUG:
         log.info("当前处于调试状态，不进行短信验证码验证...")
-        return settings.SMS_DEBUG_CODE
+        return False
 
     data = {'mobilePhoneNumber': mobile}
     log.info('requestSms: %s', mobile)
@@ -29,7 +29,7 @@ def request_sms(mobile):
     log.info('headers: %s', headers)
     if not settings.LEANCLOUD_PUSH_ENABLED:
         log.warn('没有打开短信验证码功能!!!!')
-        return None
+        return False
 
     session = FuturesSession()
     try:
@@ -53,11 +53,13 @@ def validate_captcha(mobile, captcha):
     if settings.DEBUG or settings.TESTING:
         if captcha == settings.SMS_DEBUG_CODE:
             return True
+        log.info("调试模式验证码校验失败: 发送过来的验证码 = {} 需要校验的调试验证码 = {}".format(
+            captcha, settings.SMS_DEBUG_CODE))
         return False
 
     # 判断是否已经设置了 短信验证码关键信息
     if settings.LEANCLOUD_ID or not settings.LEANCLOUD_KEY:
-        raise RuntimeError('undefined leancloud id/key')
+        raise RuntimeError('没有设定 leancloud id/key')
 
     url = VERIFY_SMS_CODE.format(captcha=captcha)
     try:
@@ -78,6 +80,7 @@ def validate_captcha(mobile, captcha):
 # 记录当前手机号码已经发过一次验证码，存入redis 一分钟后过期
 def mobile_reach_ratelimit(mobile):
     if settings.DEBUG:
+        log.info("调试模式下，可以无限次请求验证码!")
         return False
 
     key = 'bar:ratelimit:mobile:captcha:%s' % mobile
