4f617ccc179715a6f2cde5260ea5a7e0782092dc
youfeng <youfeng@haizhi.com>
2017-09-26 11:17:24 +0800

    返回字段做调整

--- tools/wx_pay.py
+++ tools/wx_pay.py
@@ -182,7 +182,7 @@ class WxPay(object):
                    nonceStr=nonce_str, package=package, signType="MD5")
         sign = self.sign(raw)
         return dict(package=package, appId=self.WX_APP_ID,
-                    timeStamp=timestamp, nonceStr=nonce_str,
+                    timeStamp=str(timestamp), nonceStr=nonce_str,
                     signType="MD5", paySign=sign)
 
     def order_query(self, **data):
