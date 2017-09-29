688f4b79593e813da69cf5fc7a6c8b7057fb6bf9
youfeng <youfeng@haizhi.com>
2017-09-29 18:53:26 +0800

    上线测试版本

--- settings.py
+++ settings.py
@@ -7,8 +7,8 @@
 @file: settings.py
 @time: 2017/9/4 20:30
 """
-DEBUG = True
-TESTING = True
+DEBUG = False
+TESTING = False
 MOCK = False
 SECRET_KEY = "4&^^%%$%BJHGFGHHVVBN%$$#^"
 SQLALCHEMY_DATABASE_URI = 'mysql://root:000000@localhost:3306/share_bar_db?charset=utf8'
@@ -22,8 +22,8 @@ WECHAT_APP_SECRET = "9ed565f37130030341086824b2eeca43"
 WECHAT_MCH_ID = "1488747842"
 
 # 蓝灯sms
-LEANCLOUD_ID = None
-LEANCLOUD_KEY = None
+LEANCLOUD_ID = 'F8LeTHzNlAoev077YP0iTD1f-gzGzoHsz'
+LEANCLOUD_KEY = '4CaExa2BOpO5t5USckWade8i'
 # sms 短信开关，是否使用
-LEANCLOUD_PUSH_ENABLED = False
+LEANCLOUD_PUSH_ENABLED = True
 SMS_DEBUG_CODE = '123456'
