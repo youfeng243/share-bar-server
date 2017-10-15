77d7f358451f450f5ad487a400db81571ab14db4
youfeng <youfeng@haizhi.com>
2017-10-15 18:52:46 +0800

    增加获取用户信息脚本

---
+++ test/get_user_info.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+@author: youfeng
+@email: youfeng243@163.com
+@license: Apache Licence
+@file: get_user_info.py
+@time: 2017/10/15 18:51
+"""
+from tools.wechat_api import get_wechat_user_info
+
+if __name__ == '__main__':
+    print get_wechat_user_info('oU6SZ0rHqeAKqFWWN3mnDkjDGkdc')
