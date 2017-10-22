6da4a069c9993f4d564171dc26e3df58d8de444a
youfeng <youfeng@haizhi.com>
2017-10-22 23:33:32 +0800

    添加模拟心跳客户端

--- test/simulate_client.py
+++ test/simulate_client.py
@@ -9,8 +9,10 @@
 """
 
 import sys
+import time
 
 import MySQLdb
+import requests
 
 sys.path.append("..")
 
@@ -56,10 +58,22 @@ def get_all_device_code():
 
 
 def main():
+    url = 'http://weixin.doumihuyu.com/windows/check'
     while True:
         device_list = get_all_device_code()
-        # todo 发送心跳请求
 
+        for device_code in device_list:
+            try:
+                r = requests.post(url, json={'device_code': device_code})
+                log.info("当前请求状态: device_code = {} status_code = {}".format(
+                    device_code, r.status_code))
+                log.info("请求内容: text = {}".format(r.text))
+            except Exception as e:
+                log.error("发送请求失败: device_code = {}".format(device_code))
+                log.exception(e)
+
+        log.info("发送完成，休眠10s")
+        time.sleep(10)
 
 
 if __name__ == '__main__':
