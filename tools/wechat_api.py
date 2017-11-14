add4d2410bd2ef8bd7524add49d5f00c5b4a6538
youfeng <youfeng@haizhi.com>
2017-11-14 12:15:59 +0800

    用户名信息容错

--- tools/wechat_api.py
+++ tools/wechat_api.py
@@ -302,6 +302,8 @@ def get_user_wechat_info(refresh_token, openid):
             return head_img_url, nick_name
 
         head_img_url, nick_name = data.get('headimgurl', ''), data.get('nickname', '')
+        head_img_url = head_img_url.strip()
+        nick_name = nick_name.strip()
     except Exception as e:
         log.error("访问微信用户信息失败: ")
         log.exception(e)
